6c2880dd095e6ef8b59f8c710e6fd75b4e25b95a
Jan Eduardo <janeduardo@discente.ufg.br>
2022-10-17 18:31:17 -0300

    adding project

---
+++ back/src/main/java/com/nexttech/meetingroom/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.nexttech.meetingroom.repository;
+
+import com.nexttech.meetingroom.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+}
---
+++ back/src/test/java/com/nexttech/meetingroom/MeetingroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexttech.meetingroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
